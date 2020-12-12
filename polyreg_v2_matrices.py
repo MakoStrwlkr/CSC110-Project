92bf098b4e4f3c7e2894335a37e04403f2c9cadb
MakoStrwlkr <72935357+MakoStrwlkr@users.noreply.github.com>
2020-12-13 00:12:55 +0530

    Update polyreg with error calculations
    Add module docstring,
    Add r-squared,
    Add covariance,
    Add correlation,
    Add new functions,
    TODO: Add better function docstrings, doctests

--- polyreg_v2_matrices.py
+++ polyreg_v2_matrices.py
@@ -1,25 +1,28 @@
-""" CSC110 Course Project - Data Analysis
+""" CSC110 Course Project
+Data Analysis using Polynomial regression implemented using matrices through
+the ordinary least squares estimator.
 
-Polynomial regression implemented using matrices.
+Written by Arkaprava Choudhury,
+In partnership with Ching Chang, Letian Cheng, and Hanrui Fan.
 
 In contrast to v1, where the program used lists to represent polynomials,
 this program uses a different approach to finding the polynomial regression model.
 
-A simpler approach is used here, relying more on numpy's matrix feature,
-which I plan to rewrite using my own functions, similar to the transpose functions
-used in Assignment 4.
-
 The sole limitation of this model lies in calculating the errors; the complexity of
 the calculations involved increases with this approach as the number of calculations changes.
 
-Overall, I'd say this method is better, (well, since this has a complete implementation, and
-the other doesn't).
+All work presented here written by Arkaprava Choudhury, et. al, and no piece of code has been
+referenced from any sources on the Internet. The authors reserve all rights to replicate this
+work, and no one, apart from the graders of this project and the instructors of CSC110 can
+modify / use this code as their own.
 
+This file is Copyright (c) by Ching Chang, Letian Cheng, Arkaprava Choudhury, and Hanrui Fan.
 """
 
 import numpy as np
 import matplotlib.pyplot as plt
-from typing import List, Tuple, Any, Dict, Optional
+from typing import List, Tuple, Any, Dict
+import math
 
 
 ############################################################################################
@@ -28,7 +31,7 @@ from typing import List, Tuple, Any, Dict, Optional
 ############################################################################################
 
 def split_coordinates(coordinates: List[Tuple[float, float]]) -> Tuple[List[float], List[float]]:
-    """Returns a tuple of the x-values and y-values.
+    """Return a tuple of the x-values and y-values.
 
     Preconditions:
       - coordinates != []
@@ -42,7 +45,7 @@ def split_coordinates(coordinates: List[Tuple[float, float]]) -> Tuple[List[floa
 ############################################################################################
 
 def transpose_matrix(matrix: List[List[float]]) -> List[List[float]]:
-    """Returns the transpose of the input matrix.
+    """Return the transpose of the input matrix.
 
     Note: will work for only real inputs, so cannot extend this to
     complex polynomials.
@@ -53,6 +56,15 @@ def transpose_matrix(matrix: List[List[float]]) -> List[List[float]]:
 
     return transpose
 
+
+############################################################################################
+# Initially, I had planned on using numpy's matrix multiplication function, which is much more
+# efficient than the naive approach I used.
+# Although I know how to make this implementation faster using the module tensorflow, the exact
+# algorithm used is beyond the scope of the course or any other math courses I have taken, and
+# as such, I was not fully convinced of its correctness, and so, did not use it.
+# For the purposes of this project, a Theta(n^2) algorithm is sufficient.
+############################################################################################
 
 # def matrix_multiplication_numpy(matrix_1: List[List[float]], matrix_2: List[List[float]]) \
 #         -> List[List[float]]:
@@ -118,8 +130,11 @@ def matrix_inverse_numpy(matrix: List[List[float]]) -> Any:
 
 
 def find_coefficients(x_data: List[float], y_data: List[float], degree: int) -> List[List[float]]:
-    """Returns the coefficients of the estimate polynomial that approximates
-    the given data.
+    """Return the coefficients of the estimate polynomial that approximates
+    the given data, as per the ordinary least squares estimator function.
+
+    This method was chosen, as it minimizes the sum of the squares of residuals,
+    and consequently, it also minimizes the variance, as proven by the Gauss-Markov theorem.
 
     The list of estimated coefficients contains the coefficients of 1, x, x^2, ...
     till x^(degree).
@@ -150,12 +165,19 @@ def find_coefficients(x_data: List[float], y_data: List[float], degree: int) -> 
     return beta
 
 
+############################################################################################
+# Originally, I had planned on using a custom class Matrix represent the matrices used in this
+# file, but I decided that using List[List[float]] type annotations were a better way of
+# representing the different shapes of matrices used (column matrix, rectangular matrix, and
+# square matrix).
+# Or else, I could have used the numpy array data class, but that required me to repeatedly call
+# the tolist() function in my own functions, so I did not choose that.
+############################################################################################
+
 # class Matrix:
 #     """An abstract data class for a nested list representation of a matrix.
 #     All subclasses support matrix transpose, matrix inverse, matrix multiplication.
 #
-#     Public Instance Attributes:
-#       - matrix: nested list representation of a matrix.
 #     """
 #
 #     ...
@@ -181,9 +203,12 @@ def find_coefficients(x_data: List[float], y_data: List[float], degree: int) -> 
 #         """
 #         self.matrix = make_matrix(points, degree)
 
+############################################################################################
+# This function is used to make the matrix X in the matrix equation specified above.
+############################################################################################
 
 def make_matrix(points: List[float], degree: int) -> List[List[float]]:
-    """Returns a nested list representation of a matrix consisting of the basis
+    """Return a nested list representation of a matrix consisting of the basis
     elements of the polynomial of degree n, evaluated at each of the points.
 
     In other words, each row consists of 1, x, x^2, ..., x^n, where n is the degree,
@@ -220,28 +245,30 @@ class PolynomialRegression:
       - x_values:
       - y_values:
       - error_values:
-      -
+      - r_squared:
 
     Representation invariants:
-      - coefficients != []
-      - x_values != []
-      - y_values != []
-      - len(x_values) = len(y_values)
-      - len(coefficients) <= len(x_values)
-      - len(error_values) = len(x_values)
+      - self.coefficients != []
+      - self.x_values != []
+      - self.y_values != []
+      - len(self.x_values) = len(self.y_values)
+      - len(self.coefficients) <= len(self.x_values)
+      - len(self.error_values) = len(self.x_values)
+      - 0 <= self.r_squared <= 1
 
     """
     coefficients: List[float]
     x_values: List[float]
     y_values: List[float]
     error_values: List[float]
+    r_squared: float
 
     def __init__(self, data: List[Tuple[float, float]], degree: int) -> None:
-        """ Initializes the polynomial regression model of specified degree
+        """Initialize the polynomial regression model of specified degree
         generated by input data.
 
         coefficients is in the form [a_0, a_1, ..., a_(degree), a_(degree)]
-        
+
         Preconditions are as specified in the respective function docstrings.
         """
         x_values, y_values = split_coordinates(data)
@@ -254,9 +281,10 @@ class PolynomialRegression:
         self.y_values = y_values
         self.error_values = [self.y_values[i] - self(self.x_values[i])
                              for i in range(len(self.x_values))]
+        self.r_squared = self.find_r_squared()
 
     def __call__(self, x) -> float:
-        """A method to call the polynomial
+        """A method to call the polynomial.
         """
         evaluation = 0
         for index, coefficient in enumerate(self.coefficients):
@@ -273,7 +301,48 @@ class PolynomialRegression:
         plt.show()
 
     def extreme_absolute_error(self) -> Dict[str, float]:
-        """Returns a dictionary of minimum absolute error: value
+        """Return a dictionary of minimum absolute error: value
         and maximum absolute error: value
         """
         return {'min error': min(self.error_values), 'max error': max(self.error_values)}
+
+    def find_r_squared(self) -> float:
+        """Return the coefficient of determination for the polynomial regression model
+        """
+        mean_y = expected_value(self.y_values)
+        total_sum = sum([(y - mean_y) ** 2 for y in self.y_values])
+        residual_sum = sum([(self.y_values[i] - self(self.x_values[i])) ** 2
+                            for i in range(len(self.x_values))])
+
+        return 1 - (residual_sum / total_sum)
+
+    def covariance_with_polynomial(self) -> float:
+        """Return the covariance of the y_values in the data, and the polynomial model. """
+        product_list = [self.y_values[i] * self(self.x_values[i])
+                        for i in range(len(self.x_values))]
+        expected_product = expected_value(product_list)
+        expected_polynomial = expected_value([self(self.x_values[i])
+                                              for i in range(len(self.x_values))])
+        return expected_product / (expected_value(self.y_values) * expected_polynomial)
+
+    def correlation_of_data(self) -> float:
+        """Return the correlation of the y_values in the data, with the x_values. """
+        product_list = [self.y_values[i] * self.x_values[i]
+                        for i in range(len(self.x_values))]
+        expected_product = expected_value(product_list)
+        expected_x_expected_y = expected_value(self.y_values) * expected_value(self.x_values)
+        numerator = expected_product / expected_x_expected_y
+        denominator = standard_deviation(self.y_values) * standard_deviation(self.x_values)
+        return numerator / denominator
+
+
+def expected_value(values: List[float]) -> float:
+    """Return the expected value of the input list"""
+    return sum(values) / len(values)
+
+
+def standard_deviation(values: List[float]) -> float:
+    """Return the standard deviation of the input list"""
+    mean_val = expected_value(values)
+    sum_of_squares = sum([(val - mean_val) ** 2 for val in values])
+    return math.sqrt(sum_of_squares / (len(values) - 1))
