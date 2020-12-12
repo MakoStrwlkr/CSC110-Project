ada329c28738aafd284277c1160c9c73c91f0dd8
JerryFan <70797927+HanruiJerryFan@users.noreply.github.com>
2020-12-13 05:13:53 +0800

    Test

--- rainfall_read.py
+++ rainfall_read.py
@@ -1,63 +1,9 @@
-# from mpl_toolkits.basemap import Basemap, cm
-# import matplotlib.pyplot as plt
+from getdata import Climate
 import numpy as np
 from pyhdf.SD import SD, SDC
 import csv
 import os
-from typing import Any, List, Tuple
-import datetime
-
-
-class Climate:
-    """A class that save all of the precipitation data by longitude and latitude
-
-        Public Attributes:
-            - name: the name of this Climate dataset
-            - date: a datetime object record date
-            - stored_data: the precipitation/co2emission data for given time
-            - data: a list of float corresponding to area added
-            - average: a float represent the average of precipitation in the area
-            - max: a float represent the max of precipitation/co2 in the area
-            - min: a float represent the min of precipitation/co2 in the area
-
-        Representation Invariants:
-            - self.name != ''
-    """
-    name: str
-    date: datetime.date
-    stored_data: Any
-    area_data: List[float]
-    average: float
-    max: float
-    min: float
-
-    def __init__(self, name: str, date: datetime.date, stored_data: Any) -> None:
-        """Initialize a new precipitation dataset
-
-        The dataset starts with no data
-        """
-        self.name = name
-        self.date = date
-        self.stored_data = stored_data
-        self.area_data = []
-        self.average = 0.0
-        self.max = 0.0
-        self.min = 0.0
-
-    def add_area(self, location: Any) -> None:
-        """Add the area that needed to be examined.
-
-        """
-        raise NotImplementedError
-
-    def renew_property(self) -> None:
-        """Renew max, min and average of data
-
-        """
-        import statistics
-        self.max = max(self.area_data)
-        self.min = min(self.area_data)
-        self.average = statistics.mean(self.area_data)
+from typing import Any, List, Tuple, Dict
 
 
 class Precipitation(Climate):
@@ -66,17 +12,17 @@ class Precipitation(Climate):
     Private Attributes:
         - area: a list of list contain latitude and longitude corresponding x and y
     """
-    _area: List[Tuple[int, int]]
+    data: Dict[Tuple[int, int], List[float]]
 
-    def __init__(self, name: str, date: datetime.date, stored_data: Any) -> None:
+    def __init__(self, name: str, year: int) -> None:
         """Initialize a new precipitation dataset
 
         The dataset starts with no data
         """
-        Climate.__init__(self, name, date, stored_data)
-        self._area = []
+        Climate.__init__(self, name, year)
+        self.data = {}
 
-    def add_area(self, location: Tuple[float, float]) -> None:
+    def add_area(self, location: Tuple[float, float], filepath: str) -> None:
         """Add the given location(Tuple) responding grid by 0.25°*0.25° in scope of 50°S to 50°N and 180°W – 180°E
         S is negative and W correspond to negative value.
 
@@ -93,53 +39,44 @@ class Precipitation(Climate):
 
         x = int((-(latitude - 50)) // 0.25)
         y = int((longitude + 180) // 0.25)
-        location = (x, y)
 
-        if location in self._area:
+        if (x, y) in self.data:
             print(str((latitude, longitude)) + ' already exists.')
         else:
-            self._area.append(location)
+            self.read_file(filepath, x, y)
             print(str((latitude, longitude)) + ' added.')
 
-            temp_precip = self.stored_data[x][y]
-            self.area_data.append(temp_precip)
-            self.renew_property()
+    def read_file(self, filepath: str, x: int, y: int) -> None:
+        path_dir = os.listdir(filepath)
+        cur = 0
 
+        for s in path_dir:
+            new_dir = os.path.join(filepath, s)
+            if os.path.isfile(new_dir)\
+                    and os.path.splitext(new_dir)[1].lower() == ".hdf"\
+                    and str(self.year) in s\
+                    and cur == 0:
+                self.data[(x, y)] = (np.transpose(SD(new_dir, SDC.READ).select('precipitation')[:])[x][y])
+                cur = 1
+            elif os.path.isfile(new_dir) \
+                    and os.path.splitext(new_dir)[1].lower() == ".hdf" \
+                    and str(self.year) in s\
+                    and cur == 1:
+                temp_data = []
+                temp_data.extend(np.transpose(SD(new_dir, SDC.READ).select('precipitation')[:]))
+                self.data[(x, y)].append(temp_data[x][y])
 
-def read_each_file(filepath: str, file_class: str) -> list:
-    """Read file under folder of filepath
+    def get_value(self) -> None:
+        num_point = len(self.data)
+        num_month = 12
+        sum_so_far = 0
+        for point in self.data:
+            sum_so_far += self.data[point]
 
-    """
-    path_dir = os.listdir(filepath)
-    data_base = []
-
-    for s in path_dir:
-        new_dir = os.path.join(filepath, s)
-        if os.path.isfile(new_dir)\
-                and os.path.splitext(new_dir)[1].lower() == ".hdf"\
-                and file_class.lower() == 'precipitation':
-            date = os.path.splitext(new_dir)[0].split('.')[1]
-            data_base.append(Precipitation('Precipitation' + date,
-                                          datetime.date(int(date[0:4]), int(date[4:6]), int(date[6:8])),
-                                          read_hdf(new_dir, file_class.lower())))
-
-    return data_base
-
-
-def read_hdf(filepath: str, file_class: str) -> Any:
-    """Read HDF File by file_class with update compatibility if there are multiple file_class
-
-    """
-    if file_class == 'precipitation':
-        precipitation_dataset = np.transpose(SD(filepath, SDC.READ).select('precipitation')[:])
-        return precipitation_dataset
+        self.value = sum_so_far / num_month / num_point
 
 
 if __name__ == '__main__':
-    data1 = np.transpose(SD('3B43_rainfall/3B43.20040201.7A.HDF', SDC.READ).select('precipitation')[:])
-    temp1 = Precipitation('AmazonPrecipitation20040201', datetime.date(2004, 2, 1), data1)
-    temp1.add_area((50, -180))
-    temp1.add_area((50, -179.75))
-    temp1.add_area((50, -179.49))
-    temp1.add_area((50, -179.76))
-    dataset1 = read_each_file('3B43_rainfall', 'precipitation')
+    temp1 = Precipitation('AmazonPrecipitation20040201', 2004)
+    temp1.add_area((50, -180), '3B43_rainfall')
+    temp1.add_area((50, -179.75), '3B43_rainfall')
