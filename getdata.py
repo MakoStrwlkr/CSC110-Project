283c9df31ca80f6a5b531882165521a4ba845c53
ReakFakeAccount <63186767+RealFakeAccount@users.noreply.github.com>
2020-12-13 12:24:25 -0500

    pyta mistakes in getdata fixed

--- getdata.py
+++ getdata.py
@@ -1,11 +1,12 @@
 """ CSC110 Course Project - Read Data and Store with Defined Type
 
 """
-import numpy as np
-from pyhdf.SD import SD, SDC
 import csv
 import os
-from typing import List, Tuple, Any
+from typing import Any, List, Tuple
+
+import numpy as np
+from pyhdf.SD import SD, SDC
 
 
 class Climate:
@@ -48,23 +49,25 @@ def precipitation_read_hdf(filepath: str,
             - leftup[1] > rightbottom[1]
     """
     res = list()
-    lat1 = leftup[0]
-    lon1 = leftup[1]
-    lat2 = rightbottom[0]
-    lon2 = rightbottom[1]
-    if lat1 == -50:
-        lat1 += 0.01
-    if lon1 == 180:
-        lon1 = -lon1
-    if lat2 == -50:
-        lat2 += 0.01
-    if lon2 == 180:
-        lon2 = -lon2
+    # lat1 = leftup[0]
+    # lon1 = leftup[1]
+    # lat2 = rightbottom[0]
+    # lon2 = rightbottom[1]
+    if leftup[0] == -50:
+        leftup[0] += 0.01
+    if leftup[1] == 180:
+        leftup[1] = -leftup[1]
+    if rightbottom[0] == -50:
+        rightbottom[0] += 0.01
+    if rightbottom[1] == 180:
+        rightbottom[1] = -rightbottom[1]
 
-    x1 = int((-(lat1 - 50)) // 0.25)
-    y1 = int((lon1 + 180) // 0.25)
-    x2 = int((-(lat2 - 50)) // 0.25)
-    y2 = int((lon2 + 180) // 0.25)
+    # x1 = int((-(leftup[0] - 50)) // 0.25)
+    # y1 = int((leftup[1] + 180) // 0.25)
+    # x2 = int((-(rightbottom[0] - 50)) // 0.25)
+    # y2 = int((rightbottom[1] + 180) // 0.25)
+    P1 = (int((-(leftup[0] - 50)) // 0.25), int((leftup[1] + 180) // 0.25))
+    P2 = (int((-(rightbottom[0] - 50)) // 0.25), int((rightbottom[1] + 180) // 0.25))
 
     path_dir = os.listdir(filepath)
     stored_data = {}
@@ -75,26 +78,27 @@ def precipitation_read_hdf(filepath: str,
                 and year not in stored_data:
             temp = np.transpose(SD(new_dir, SDC.READ).select('precipitation')[:])
             sum_so_far = 0.0
-            for i in range(x1, x2 + 1):
-                for j in range(y1, y2 + 1):
+            for i in range(P1[0], P2[0] + 1):
+                for j in range(P1[1], P2[1] + 1):
                     sum_so_far += temp[i][j]
             stored_data[year] = sum_so_far
 
         elif os.path.splitext(new_dir)[1].lower() == ".hdf":
             temp = np.transpose(SD(new_dir, SDC.READ).select('precipitation')[:])
             sum_so_far = 0.0
-            for i in range(x1, x2 + 1):
-                for j in range(y1, y2 + 1):
+            for i in range(P1[0], P2[0] + 1):
+                for j in range(P1[1], P2[1] + 1):
                     sum_so_far += temp[i][j]
             stored_data[year] += sum_so_far
     for year in stored_data:
-        res.append(Climate('Amazon Precipitation', int(year), stored_data[year] / 12 / (x2 - x1 + 1) / (y2 - y1 + 1)))
+        res.append(Climate('Amazon Precipitation', int(year), stored_data[year] / 12 / (P2[0] - P1[0] + 1) / (P2[1] - P1[1] + 1)))
 
     return res
 
 
 def co2_read_csv(filepath: str, country: str) -> Any:
     """read CO2 data from given csv file.
+    In this project, you should always choose country as Brazil.
     """
     res = list()
     with open(filepath, newline='') as csvfile:
@@ -109,7 +113,15 @@ def co2_read_csv(filepath: str, country: str) -> Any:
 
 
 def deforestation_read_csv(filepath: str, row_name: str) -> List[Climate]:
-    """read deforestation from given csv file
+    """read deforestation from given csv file.
+    You can choose the row name from the following:
+
+    Period
+    Estimated Natural Forest Cover
+    Deforestation (INPE)
+    Natural forest cover change
+    Forest cover as % of pre-1970 cover
+    Total forest loss since 1970
     """
     res = list()
     with open(filepath, newline='') as csvfile:
@@ -165,5 +177,21 @@ def read_data_from_csv(filename: str) -> List[Climate]:
 
 
 if __name__ == '__main__':
-    save_data_as_csv(getdata(), "dataset.csv")
+    # save_data_as_csv(getdata(), "dataset.csv")
     # read_data_from_csv("dataset.csv")
+
+    import doctest
+
+    doctest.testmod(verbose=True)
+
+    import python_ta
+
+    python_ta.check_all(config={
+        'extra-imports': ['numpy', 'matplotlib.pyplot', 'typing', 'math',
+                          "pyhdf.SD", "pyhdf.SDC", "csv", "os"],
+        'allowed-io': ["read_data_from_csv", "save_data_as_csv",
+                       "deforestation_read_csv", "co2_read_csv"],  # the names (strs) of functions that call print/open/input
+        'max-line-length': 150,
+        'disable': ['R1705', 'C0200'],
+        'max-nested-blocks': 5
+    })
